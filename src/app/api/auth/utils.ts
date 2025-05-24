741277cdd3e17c2617f70d1c43c10b10f7618275
Toufiquer <toufiquerabc@gmail.com>
2025-05-25 02:58:11 +0600

    done @ build 4

--- src/app/api/auth/utils.ts
+++ src/app/api/auth/utils.ts
@@ -1,8 +1,10 @@
 import Users from '@/app/api/auth/[...nextauth]/google-auth-modal';
-
+import connectDB from '@/lib/mongoose';
 // Function to check if the email exists in the database
 export async function checkEmail(email: string) {
+  await connectDB();
   console.log('inside check mail');
   const authRecord = await Users.findOne({ email });
+  console.log('end check mail');
   return authRecord;
 }
